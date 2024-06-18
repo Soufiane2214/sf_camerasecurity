5f00fdcc00be621b91a31de9e444cde20e29c305
Soufiane <167934969+Soufiane2214@users.noreply.github.com>
2024-06-18 15:06:22 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,10 +4,10 @@ name 'sf_camerasecurity'
 author 'Soufiane'
 lua54 'yes'
 description 'Advanced Camera security script'
-version '1.4.1'
+version '1.4.2'
 
 ui_page 'html/index.html'
-files {'html/index.html', 'html/vue.min.js', 'html/script.js', 'html/vcr-ocd.ttf'}
+files {'html/index.html', 'html/vue.min.js', 'html/script.js', 'html/vcr-ocd.ttf', 'html/images/*.*'}
 
 shared_scripts {'@ox_lib/init.lua', 'shared/functions.lua', 'config.lua'}
 client_scripts {'client/utils.lua', 'bridge/**/client.lua', 'client/main.lua'} 
