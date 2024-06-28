469e4d2648c1736c1e4e3e7106e726f092c6a64f
Soufiane <167934969+Soufiane2214@users.noreply.github.com>
2024-06-28 01:04:14 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,7 +10,7 @@ ui_page 'html/index.html'
 files {'html/index.html', 'html/vue.min.js', 'html/script.js', 'html/vcr-ocd.ttf', 'html/images/*.*'}
 
 shared_scripts {'@ox_lib/init.lua', 'shared/functions.lua', 'config.lua'}
-client_scripts {'client/utils.lua', 'bridge/**/client.lua', 'client/main.lua'} 
+client_scripts {'bridge/**/client.lua', 'client/utils.lua',  'client/main.lua'} 
 server_scripts {'@oxmysql/lib/MySQL.lua', 'bridge/**/server.lua', 'server/main.lua'}
 
 dependencies {'oxmysql', 'ox_lib'}
